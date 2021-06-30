be6613b2678c669c587291535eda32ef801e7b6d
Maurice van Creij <maurice.vancreij@webqem.com>
2021-06-30 11:02:55 +1000

    Initial commit

---
+++ tampermonkey.js
@@ -0,0 +1,75 @@
+// ==UserScript==
+// @name         Fast CSS reload
+// @namespace    http://tampermonkey.net/
+// @version      0.1.0
+// @description  Cache-bust all the things
+// @author       maurice.vancreij@webqem.com
+// @include      *://*
+// @grant        GM_xmlhttpRequest
+// @connect      localhost
+// ==/UserScript==
+
+(function() {
+
+  'use strict';
+
+  // CONDITIONS
+
+  if (/admin/.test(document.location.href)){ return false; }
+  else { window.tampered = true; }
+
+  // PROPERTIES
+
+  const rule = 'link[href], img[src]';
+  const swap = /domain.tld\/path\/file.ext/i;
+  const local = 'localhost/path/file.ext';
+  const css = 'localhost/path/file.ext';
+  const js = 'localhost/path/file.ext';
+
+  // METHODS
+
+  function insert() {
+    if (css && css !== '') {
+      var link = document.createElement('link');
+      link.setAttribute('rel', 'stylesheet');
+      link.setAttribute('href', css);
+      document.head.appendChild(link);
+    }
+    if (js && js !== '') {
+      var script = document.createElement('script');
+      script.setAttribute('src', js);
+      document.head.appendChild(script);
+    }
+  }
+
+  function bustCaches() {
+    var url, type;
+    var domain = document.location.protocol + '//' + document.domain + '/';
+    var time = new Date().getTime();
+    var elems = [...document.querySelectorAll(rule)];
+    var path = '';
+    elems.map(elem => {
+      type = (elem.getAttribute('href')) ? 'href' : 'src';
+      try {
+        url = new URL(elem.getAttribute(type), domain);
+        url.searchParams.set('t', time);
+        path = url.href.replace(swap, local);
+        elem.setAttribute(type, path);
+      } catch (e) {
+        console.log(e, elem.getAttribute(type));
+      }
+    });
+  };
+
+  // EVENTS
+
+  insert();
+  bustCaches();
+
+  window.addEventListener('keyup', function(evt) {
+    if (evt.key === '`') {
+      bustCaches();
+    }
+  });
+
+})();
